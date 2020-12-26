f1a0da0faf9d1875a19a3d7baa5f7f6b8df402b5
Imaya Kumar Jagannathan <ijaganna@amazon.com>
2020-12-25 19:04:02 -0500

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Jagannathan, Imaya Kumar on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
